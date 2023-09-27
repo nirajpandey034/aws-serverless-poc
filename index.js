c8e8fdc276d6b21b1e2a4b0e5393275c19049578
Niraj Pandey <nirajpandey034@gmail.com>
2023-09-27 13:25:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
+
+app.get("/path", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+// app.listen(3000, () => {
+//   console.log("Listening");
+// });
+module.exports.handler = serverless(app);
